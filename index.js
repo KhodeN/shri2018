20c1d647870b9f818216a58cafe8b02159567692
sasha <sasha@localhost.localdomain>
2018-02-01 06:37:05 +0300

    cors

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const graphqlRoutes = require('./graphql/routes');
 
 const app = express();
 
+const cors = require('cors');
 app.use(bodyParser.json());
 
 app.use('/', pagesRoutes);
-app.use('/graphql', graphqlRoutes);
+app.use('/graphql', cors(), graphqlRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => console.log('Express app listening on localhost:3000'));
